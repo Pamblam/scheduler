817ee663c57325b99cd2506e5be7a2b9a068c1a8
unknown <=>
2025-03-12 20:54:30 -0400

    autocommit

--- Scheduler/obj/Release/net8.0-windows/Scheduler.AssemblyInfo.cs
+++ Scheduler/obj/Release/net8.0-windows/Scheduler.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scheduler")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd6d0d93d6df63ff47140843edac7da5ea94836a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scheduler")]
